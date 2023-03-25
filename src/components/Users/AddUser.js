18a7cd87fdad853d92109a2bd148689c6e95e4ee
ColtonRohan <crohan2020@fau.edu>
2023-03-25 19:07:28 -0400

    CSS for Login Screen

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,38 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import { useState } from "react";
+
+const defaultUser = {
+  username: "Colton",
+  age: 24,
+};
 
 const AddUser = (props) => {
+  const [newUser, setNewUser] = useState("");
+  const [newUserAge, setNewUserAge] = useState("");
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(newUser, newUserAge);
   }
 
+  const newUserChangeHandler = (event) => {
+    setNewUser(event.target.value);
+  };
+
+  const newUserAgeChangeHandler = (event) => {
+    setNewUserAge(event.target.value);
+  };
+
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={newUserChangeHandler} />
         <label htmlFor="age">Age ( Years )</label>
-        <input type="number" id="age" />
-        <button type="submit">Add User</button>
+        <input type="number" id="age" onChange={newUserAgeChangeHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
