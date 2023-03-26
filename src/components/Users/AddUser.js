1d716533d835b57357430276c7f9e4b52bd8c8a1
ColtonRohan <crohan2020@fau.edu>
2023-03-26 16:25:44 -0400

    Switched user input to refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,49 +2,46 @@ import React from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wraper from "../Helpers/Wraper";
 
 const AddUser = (props) => {
-  const [newUser, setNewUser] = useState("");
-  const [newUserAge, setNewUserAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [showErrorModal, setShowErrorModal] = useState();
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (newUser.trim().length === 0 || newUserAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setShowErrorModal({
         title: "Invalid Input",
         message: "Please enter a valid input",
       });
       return;
     }
-    if (+newUserAge < 1) {
+    if (+enteredAge < 1) {
       setShowErrorModal({
         title: "Invalid Age",
         message: "Please enter an age that is greater then 0",
       });
       return;
     }
-    props.onAddUser(newUser, newUserAge);
-    setNewUser("");
-    setNewUserAge("");
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   }
-
-  const newUserChangeHandler = (event) => {
-    setNewUser(event.target.value);
-  };
-
-  const newUserAgeChangeHandler = (event) => {
-    setNewUserAge(event.target.value);
-  };
 
   const errorHandler = () => {
     setShowErrorModal(null);
   };
 
   return (
-    <div>
+    <Wraper>
       {showErrorModal && (
         <ErrorModal
           title={showErrorModal.title}
@@ -55,23 +52,13 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={handleSubmit}>
           <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={newUser}
-            onChange={newUserChangeHandler}
-          />
+          <input type="text" id="username" ref={nameInputRef} />
           <label htmlFor="age">Age ( Years )</label>
-          <input
-            type="number"
-            id="age"
-            value={newUserAge}
-            onChange={newUserAgeChangeHandler}
-          />
+          <input type="number" id="age" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wraper>
   );
 };
 
