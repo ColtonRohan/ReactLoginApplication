7e1de3971d145166f057a4d0569d1df41a64aacb
ColtonRohan <crohan2020@fau.edu>
2023-03-26 13:49:31 -0400

    Added Error Modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import { UsersList } from "./components/Users/UsersList";
+
+const defaultUser = [
+  {
+    name: "Colton",
+    age: 24,
+    id: "u1",
+  },
+  {
+    name: "bill",
+    age: 224,
+    id: "u2",
+  },
+];
 
 // need to edit the user component still
 function App() {
+  const [usersList, setUsersList] = useState(defaultUser);
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prev) => {
+      return [...prev, { name: name, age: age, id: Math.random().toString() }];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,18 +3,32 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import { useState } from "react";
-
-const defaultUser = {
-  username: "Colton",
-  age: 24,
-};
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
   const [newUser, setNewUser] = useState("");
   const [newUserAge, setNewUserAge] = useState("");
+  const [showErrorModal, setShowErrorModal] = useState();
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(newUser, newUserAge);
+    if (newUser.trim().length === 0 || newUserAge.trim().length === 0) {
+      setShowErrorModal({
+        title: "Invalid Input",
+        message: "Please enter a valid input",
+      });
+      return;
+    }
+    if (+newUserAge < 1) {
+      setShowErrorModal({
+        title: "Invalid Age",
+        message: "Please enter an age that is greater then 0",
+      });
+      return;
+    }
+    props.onAddUser(newUser, newUserAge);
+    setNewUser("");
+    setNewUserAge("");
   }
 
   const newUserChangeHandler = (event) => {
@@ -25,16 +39,39 @@ const AddUser = (props) => {
     setNewUserAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setShowErrorModal(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" onChange={newUserChangeHandler} />
-        <label htmlFor="age">Age ( Years )</label>
-        <input type="number" id="age" onChange={newUserAgeChangeHandler} />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {showErrorModal && (
+        <ErrorModal
+          title={showErrorModal.title}
+          message={showErrorModal.message}
+          closeModal={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={newUser}
+            onChange={newUserChangeHandler}
+          />
+          <label htmlFor="age">Age ( Years )</label>
+          <input
+            type="number"
+            id="age"
+            value={newUserAge}
+            onChange={newUserAgeChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
